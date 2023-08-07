d4b48e99ce47df39b62d7aa63ef48bdda57b8b12
Mutisomary <marymutso2016@gmail.com>
2023-08-07 13:06:51 -0700

    Check betty;

---
+++ parser.c
@@ -0,0 +1,85 @@
+#include "shell.h"
+
+/**
+ * is_cmd - programs determineswhether a file is an executable command.
+ * @info: parameter.
+ * @path: path.
+ * Return: 0
+ */
+int is_cmd(info_t *info, char *path)
+{
+	struct stat st;
+
+	(void)info;
+	if (!path || stat(path, &st))
+		return (0);
+
+	if (st.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * dup_chars - programs forms character duplicates.
+ * @pathstr: path of a string.
+ * @start: starrting point.
+ * @stop: ending point.
+ *
+ * Return: pointer to new buffer
+ */
+char *dup_chars(char *pathstr, int start, int stop)
+{
+	static char buf[1024];
+	int a = 0, b = 0;
+
+	for (b = 0, a = start; a < stop; a++)
+		if (pathstr[a] != ':')
+			buf[b++] = pathstr[a];
+	buf[b] = 0;
+	return (buf);
+}
+
+/**
+ * find_path -program checks for command in PATH string.
+ * @info: parameter.
+ * @pathstr: PATH string.
+ * @cmd: command string to be found.
+ *
+ * Return: NULL or full command PATH.
+ */
+char *find_path(info_t *info, char *pathstr, char *cmd)
+{
+	int a = 0, curr_pos = 0;
+	char *path;
+
+	if (!pathstr)
+		return (NULL);
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	{
+		if (is_cmd(info, cmd))
+			return (cmd);
+	}
+	while (1)
+	{
+		if (!pathstr[a] || pathstr[a] == ':')
+		{
+			path = dup_chars(pathstr, curr_pos, a);
+			if (!*path)
+				_strcat(path, cmd);
+			else
+			{
+				_strcat(path, "/");
+				_strcat(path, cmd);
+			}
+			if (is_cmd(info, path))
+				return (path);
+			if (!pathstr[a])
+				break;
+			curr_pos = a;
+		}
+		a++;
+	}
+	return (NULL);
+}
