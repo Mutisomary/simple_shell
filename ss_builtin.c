f861938376c00085ba57bb2436f92be08b3c353c
Mutisomary <marymutso2016@gmail.com>
2023-08-07 13:09:09 -0700

    Betty style

---
+++ ss_builtin.c
@@ -0,0 +1,93 @@
+#include "shell.h"
+
+/**
+ * _myexit - program exits from the shell.
+ * @info: parameter.
+ *  Return: shell exits.
+ */
+int _myexit(info_t *info)
+{
+	int exitshell;
+
+	if (info->argv[1])
+	{
+		exitshell = _erratoi(info->argv[1]);
+		if (exitshell == -1)
+		{
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return (1);
+		}
+		info->err_num = _erratoi(info->argv[1]);
+		return (-2);
+	}
+	info->err_num = -1;
+	return (-2);
+}
+
+/**
+ * _mycd - program changes the working directory.
+ * @info: parameter.
+ *  Return: 0
+ */
+int _mycd(info_t *info)
+{
+	char *s, *directory, buffer[1024];
+	int chdir_ret;
+
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!info->argv[1])
+	{
+		directory = _getenv(info, "HOME=");
+		if (!directory)
+			chdir_ret =
+				chdir((directory = _getenv(info, "PWD=")) ? directory : "/");
+		else
+			chdir_ret = chdir(directory);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!_getenv(info, "OLDPWD="))
+		{
+			_puts(s);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		chdir_ret =
+			chdir((directory = _getenv(info, "OLDPWD=")) ? directory : "/");
+	}
+	else
+		chdir_ret = chdir(info->argv[1]);
+	if (chdir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
+}
+
+/**
+ * _myhelp - change working directory.
+ * @info: parameter.
+ *  Return: 0
+ */
+int _myhelp(info_t *info)
+{
+	char **arg_array;
+
+	arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_array);
+	return (0);
+}
