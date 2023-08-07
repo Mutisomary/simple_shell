798bd87c4faa6074d982f2298b2e062c8b5b65a2
Mutisomary <marymutso2016@gmail.com>
2023-08-07 13:10:12 -0700

    intiial commit

---
+++ ss_string1.c
@@ -0,0 +1,84 @@
+#include "shell.h"
+
+/**
+ * _strcpy - copy a string
+ * @dest: destination
+ * @src: source
+ *
+ * Return: pointer.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[a])
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	dest[a] = 0;
+	return (dest);
+}
+
+/**
+ * _strdup - funnction duplicates a string
+ * @str: string.
+ * Return: pointer.
+ */
+char *_strdup(const char *str)
+{
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}
+
+/**
+ * _puts - program dispays the  input string
+ * @str: string.
+ * Return: 0.
+ */
+void _puts(char *str)
+{
+	int a = 0;
+
+	if (!str)
+		return;
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+}
+
+/**
+ * _putchar - prints character.
+ * @c: character.
+ * Return: 0 or 1.
+ */
+int _putchar(char c)
+{
+	static int a;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || a >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, a);
+		a = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[a++] = c;
+	return (1);
+}
+
