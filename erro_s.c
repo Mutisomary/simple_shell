767186e8ef30dd3f9c19027e372ff38f5af8aa39
Mutisomary <marymutso2016@gmail.com>
2023-08-07 12:27:28 -0700

    check errors

---
+++ erro_s.c
@@ -0,0 +1,139 @@
+#include "shell.h"
+
+/**
+ * _erratoi - program converts a string to int.
+ * @s: the string.
+ * Return: 0 or -1.
+ */
+int _erratoi(char *s)
+{
+	int a = 0;
+	unsigned long int result = 0;
+
+	if (*s == '+')
+		s++;
+	for (a = 0;  s[a] != '\0'; a++)
+	{
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			result *= 10;
+			result += (s[a] - '0');
+			if (result > INT_MAX)
+				return (-1);
+		}
+		else
+			return (-1);
+	}
+	return (result);
+}
+
+/**
+ * print_error - program displays an error message
+ * @info: parameter.
+ * @estr: string with error.
+ * Return: 0 or -1.
+ */
+void print_error(info_t *info, char *estr)
+{
+	_eputs(info->fname);
+	_eputs(": ");
+	print_d(info->line_count, STDERR_FILENO);
+	_eputs(": ");
+	_eputs(info->argv[0]);
+	_eputs(": ");
+	_eputs(estr);
+}
+
+/**
+ * print_d - prints an integer,
+ * @input: parameter.
+ * @fd: filedescriptor.
+ *
+ * Return: character number.
+ */
+int print_d(int input, int fd)
+{
+	int (*__putchar)(char) = _putchar;
+	int a, count = 0;
+	unsigned int _abs_, current;
+
+	if (fd == STDERR_FILENO)
+		__putchar = _eputchar;
+	if (input < 0)
+	{
+		_abs_ = -input;
+		__putchar('-');
+		count++;
+	}
+	else
+		_abs_ = input;
+	current = _abs_;
+	for (a = 1000000000; a > 1; a /= 10)
+	{
+		if (_abs_ / a)
+		{
+			__putchar('0' + current / a);
+			count++;
+		}
+		current %= a;
+	}
+	__putchar('0' + current);
+	count++;
+
+	return (count);
+}
+
+/**
+ * convert_number - program converts a function.
+ * @num: parameter 1.
+ * @base: parameter 2.
+ * @flags: flag arguments.
+ *
+ * Return: string
+ */
+char *convert_number(long int num, int base, int flags)
+{
+	static char *array;
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
+
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
+	{
+		n = -num;
+		sign = '-';
+
+	}
+	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do	{
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
+
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
+}
+
+/**
+ * remove_comments - program replaces comments.
+ * @buf: string address.
+ *
+ * Return: 0.
+ */
+void remove_comments(char *buf)
+{
+	int a;
+
+	for (a = 0; buf[a] != '\0'; a++)
+		if (buf[a] == '#' && (!a || buf[a - 1] == ' '))
+		{
+			buf[a] = '\0';
+			break;
+		}
+}
+
