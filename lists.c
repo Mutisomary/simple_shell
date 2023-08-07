53f89185490cba2aa722ec1f00c4cf3b4b535039
Mutisomary <marymutso2016@gmail.com>
2023-08-07 13:05:44 -0700

    initial commit

---
+++ lists.c
@@ -0,0 +1,156 @@
+#include "shell.h"
+
+/**
+ * add_node - program addsa node tostart point.
+ * @head: pointer address.
+ * @str: pointer.
+ * @num: parameter.
+ * Return: list size.
+ */
+list_t *add_node(list_t **head, const char *str, int num)
+{
+	list_t *new_head;
+
+	if (!head)
+		return (NULL);
+	new_head = malloc(sizeof(list_t));
+	if (!new_head)
+		return (NULL);
+	_memset((void *)new_head, 0, sizeof(list_t));
+	new_head->num = num;
+	if (str)
+	{
+		new_head->str = _strdup(str);
+		if (!new_head->str)
+		{
+			free(new_head);
+			return (NULL);
+		}
+	}
+	new_head->next = *head;
+	*head = new_head;
+	return (new_head);
+}
+
+/**
+ * add_node_end - programs addsa node at list end.
+ * @head: pointer.
+ * @str: pointer.
+ * @num: parameter.
+ * Return: list size.
+ */
+list_t *add_node_end(list_t **head, const char *str, int num)
+{
+	list_t *new_node, *node;
+
+	if (!head)
+		return (NULL);
+
+	node = *head;
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+	_memset((void *)new_node, 0, sizeof(list_t));
+	new_node->num = num;
+	if (str)
+	{
+		new_node->str = _strdup(str);
+		if (!new_node->str)
+		{
+			free(new_node);
+			return (NULL);
+		}
+	}
+	if (node)
+	{
+		while (node->next)
+			node = node->next;
+		node->next = new_node;
+	}
+	else
+		*head = new_node;
+	return (new_node);
+}
+
+/**
+ * print_list_str - entry point.
+ * @a: pointer.
+ *
+ * Return: size of list
+ */
+size_t print_list_str(const list_t *a)
+{
+	size_t b = 0;
+
+	while (a)
+	{
+		_puts(a->str ? a->str : "(nil)");
+		_puts("\n");
+		a = a->next;
+		b++;
+	}
+	return (b);
+}
+
+/**
+ * delete_node_at_index - program deletes a node.
+ * @head: pointer address.
+ * @index: parameter.
+ * Return: 1 or 0.
+ */
+int delete_node_at_index(list_t **head, unsigned int index)
+{
+	list_t *node, *prev_node;
+	unsigned int a = 0;
+
+	if (!head || !*head)
+		return (0);
+
+	if (!index)
+	{
+		node = *head;
+		*head = (*head)->next;
+		free(node->str);
+		free(node);
+		return (1);
+	}
+	node = *head;
+	while (node)
+	{
+		if (a == index)
+		{
+			prev_node->next = node->next;
+			free(node->str);
+			free(node);
+			return (1);
+		}
+		a++;
+		prev_node = node;
+		node = node->next;
+	}
+	return (0);
+}
+
+/**
+ * free_list - program frees nodes list.
+ * @head_ptr: addres.
+ *
+ * Return: 0
+ */
+void free_list(list_t **head_ptr)
+{
+	list_t *node, *next_node, *head;
+
+	if (!head_ptr || !*head_ptr)
+		return;
+	head = *head_ptr;
+	node = head;
+	while (node)
+	{
+		next_node = node->next;
+		free(node->str);
+		free(node);
+		node = next_node;
+	}
+	*head_ptr = NULL;
+}
