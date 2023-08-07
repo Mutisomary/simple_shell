36475ce023d35cae7e60abcc30db560f649b3ab8
Mutisomary <marymutso2016@gmail.com>
2023-08-07 12:26:23 -0700

    initial commit

---
+++ enviro.c
@@ -0,0 +1,88 @@
+#include "shell.h"
+
+/**
+ * _myenv - displays the current environment
+ * @info: parameter.
+ * Return: Always 0
+ */
+int _myenv(info_t *info)
+{
+	print_list_str(info->env);
+	return (0);
+}
+
+/**
+ * _getenv - programs displays the environment variable value.
+ * @info: parameter.
+ * @name: name of the environment.
+ *
+ * Return: value
+ */
+char *_getenv(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *p;
+
+	while (node)
+	{
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * _mysetenv - new environment variable initialization.
+ * @info: parameter.
+ *  Return: Always 0
+ */
+int _mysetenv(info_t *info)
+{
+	if (info->argc != 3)
+	{
+		_eputs("Incorrect number of arguments\n");
+		return (1);
+	}
+	if (_setenv(info, info->argv[1], info->argv[2]))
+		return (0);
+	return (1);
+}
+
+/**
+ * _myunsetenv - programs removes the environment variable
+ * @info: parameter.
+ *  Return: Always 0
+ */
+int _myunsetenv(info_t *info)
+{
+	int a;
+
+	if (info->argc == 1)
+	{
+		_eputs("Too few arguments.\n");
+		return (1);
+	}
+	for (a = 1; a <= info->argc; a++)
+		_unsetenv(info, info->argv[a]);
+
+	return (0);
+}
+
+/**
+ * populate_env_list -program populates environment.
+ * @info: parameter.
+ * Return: Always 0
+ */
+int populate_env_list(info_t *info)
+{
+	list_t *node = NULL;
+	size_t a;
+
+	for (a = 0; environ[a]; a++)
+		add_node_end(&node, environ[a], 0);
+	info->env = node;
+	return (0);
+}
+
